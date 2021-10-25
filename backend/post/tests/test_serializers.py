375dbacbd0dd5daca892668674ea3359e07f13b1
JonathonZudell <jonathon.zudell@foolcontractors.com>
2021-10-25 12:43:43 -0400

    adding serialization test

--- backend/post/tests/test_serializers.py
+++ backend/post/tests/test_serializers.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
-from .serializers import PostSerializer
-from .models import Post
+from ..serializers import PostSerializer
+from ..models import Post
 class PostSerializerTestCase(TestCase):
     def setUp(self):
-        self.post = Post.objects.create({"id" : "example", "start_date" : "09/04/2022", "price" : 0})
-        self.serializers = PostSerializer()
+        self.post = Post.objects.create(**{"name" : "example", "start_date" : "09/04/2022", "price" : 0})
+        self.serializers = PostSerializer(instance=self.post)
+    
+    def test_serialize(self):
+        pass
+    
+    def test_deserialize(self):
+        pass
--- backend/post/views.py
+++ backend/post/views.py
@@ -4,6 +4,15 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
+#from django_filters import rest_framework as filters
+
+#class PostFilter(filters.FilterSet):
+#    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+#    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+#    keyword = filters.CharFilter(field_name="keyword", lookup_expr='icontains')
+#
+#    class Meta:
+#        model = Post
 
 class PostList(APIView):
     """
@@ -22,8 +31,11 @@ class PostList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class FilterPostList(generics.ListAPIView):
-    serializer_class = PostSerializer
 
+#    queryset = Product.objects.all()
+    serializer_class = PostSerializer
+#    filter_backends = (filters.DjangoFilterBackend,)
+#    filterset_class = PostFilter
     def get_queryset(self):
         """
         This view filters by keyword and prices
