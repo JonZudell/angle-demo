15d66a0f90b667e7e946a18f7432c124789c2096
JonathonZudell <jonathon.zudell@foolcontractors.com>
2021-10-26 18:14:43 -0400

    almost done

--- backend/post/tests/test_views.py
+++ backend/post/tests/test_views.py
@@ -1,4 +1,11 @@
 from django.test import TestCase
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 class PostViewTestCase(TestCase):
-    pass
+    def test_post_data_01(self):
+        json = open(staticfiles_storage.path('data_01.json'), 'rb').read()
+        try:
+            response = self.client.post('/post/', json, content_type="application/json")
+            self.assertEqual(response.status_code, 201)
+        except Exception as ex:
+            print(ex)
